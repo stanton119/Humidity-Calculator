7da790188792318481c17a0b29568e3566fc6875
Richard Stanton <stanton1191@gmail.com>
2021-08-25 13:22:59 +0100

    Moving python example

--- humidity_forecast/humidity_forecast.py
+++ humidity_forecast/humidity_forecast.py
@@ -48,10 +48,38 @@ weather_url = (
 weather_json = requests.get(weather_url).json()
 weather_dict = process_forecasts(weather_json["forecasts"])
 df = pd.DataFrame.from_dict(weather_dict, orient="index")
+df.index.name='date'
 df = df.rename(
     columns={"temperatureC": "outside_temp", "humidity": "outside_humidity"}
 )
 df["inside_humidity"] = inside_humidity(df)
 
+# %%
+df.reset_index().to_json('weather_data.json', orient='columns')
+df.to_csv('weather_data.csv')
+
+df.to_json('weather_data.json', orient='')
+{'split', 'records', 'index', 'columns', 'values', 'table'}.
+
+# %%
 fig, ax = plt.subplots(figsize=(10,6))
-df.plot(ax=ax)
+if 0:
+    axs = df.head(72).plot(ax=ax, secondary_y='outside_temp')
+    ax.set_ylabel('ºC', color='r')
+else:
+    df.head(72).loc[:,['outside_humidity','inside_humidity']].plot(ax=ax)
+if 0:
+    df.head(72).loc[:,['outside_humidity','inside_humidity']].plot(ax=ax)
+    ax2 = ax.twinx()
+    df.head(72).loc[:,['outside_temp']].plot(ax=ax2, color='r')
+    ax2.set_ylabel('ºC', color='r')
+    ax2.tick_params('y', colors='r')
+
+
+
+# import matplotlib.ticker as plticker
+# loc = plticker.MultipleLocator(base=24)
+# ax.xaxis.set_major_locator(loc)
+# loc = plticker.MultipleLocator(base=12)
+# ax.xaxis.set_minor_locator(loc)
+# ax.grid(which='major', axis='both', linestyle='-')
