14b59d11b01196c5aef04c531ae3c41ea4cb2ede
stanton119 <stanton1191@gmail.com>
2022-11-19 23:04:21 +0000

    merging bbc forecast to main.js

--- humidity_forecast/app/main.js
+++ humidity_forecast/app/main.js
@@ -28,7 +28,7 @@ function setURLParameter(parameterName, value) {
         for (const param in params) {
             page_search = page_search + param + "=" + params[param] + "&"
         }
-        page_search = page_search.slice(0,-1)
+        page_search = page_search.slice(0, -1)
     }
 
     var path = window.location.pathname;
@@ -98,6 +98,44 @@ class ForecastParams {
         setURLParameter('indoorTemp', this.indoorTemp)
         this.setForm()
     }
+}
+
+function getBBCForecast(postcode, plottingFcn) {
+    var url = "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/" + postcode;
+
+    fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return res.json();
+        })
+        .then(data => processForcast(data, standardiseBBCWeather, plottingFcn))
+        .catch(err => { throw err });
+}
+
+function standardiseBBCWeather(data) {
+    return processBBCForcasts(data['forecasts'])
+}
+
+function processBBCForcasts(forecasts) {
+    var list = []
+    for (var key in forecasts) {
+        result = processBBCReports(forecasts[key]["detailed"]["reports"])
+        list = list.concat(result)
+    }
+    return list
+}
+
+function processBBCReports(reports) {
+    var reportOutput = []
+    reports.forEach(function (report) {
+        var date = new Date(report['localDate'])
+        date.setHours(Number(report['timeslot'].substring(0, 2)))
+
+        reportOutput.push({ date: date, outside_humidity: report['humidity'], outside_temp: report['temperatureC'] })
+    })
+    return reportOutput
 }
 
 class HumidityData {
